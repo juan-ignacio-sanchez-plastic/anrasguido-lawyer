1c30f9ee367a09dfb0066bf34d9e8dd1ef597a5e
Juan Sánchez <juansp92@hotmail.com>
2021-03-23 18:16:31 -0300

    contact icons added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import whatsappLogo from '../../assets/images/whatsapp.svg';
+import emailLogo from '../../assets/images/email.svg';
+
 import './Contact.scss';
 
 const Contact = () => {
@@ -7,9 +10,19 @@ const Contact = () => {
     <address className='Contact'>
       <ul>
         <li>
+          <img
+            src={whatsappLogo}
+            width={24}
+            alt='whatsapp'
+          />
           11-3489-6281
         </li>
         <li>
+          <img
+            src={emailLogo}
+            width={24}
+            alt='email'
+          />
           rasguido@outlook.com
         </li>
       </ul>
